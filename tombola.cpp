70d9b43d8c471eb14ae2cc5dc3770949484648cc
halychmykola-prog <halych.mykola.studente@itispaleocapa.it>
2025-12-19 11:52:17 +0100

    Add files via upload
    started adding a table

--- tombola.cpp
+++ tombola.cpp
@@ -1,9 +1,55 @@
 #include <iostream>
+#include <ctime>
+#include <stdlib.h>
 using namespace std;
 
 int main()
 {
-    cout<<"Hello World";
+	int cell[15];
+	srand(time(0));
+	int column;
+	int tofill;
+	
+	for(int i=0;i<9;)
+	{
+		sosalych=rand()%9+1;
+		int help=sosalych;
+		if(==help)
+		{
+			continue;
+		}
+		else
+		{
+			i++;
+		}
+		
+		tofill=rand()%2+1;
+		for(int j=0;j<tofill;j++)
+		{
+			int count;
+			count=rand()%3;
+			
+			cell[count]=
+			
+			
+			
+		}
+		
+		
+	}
+	
+	
+	
+	
+	for(rdthetrh)
+	{
+		x[i]=y[i];
+	}
+	
+	
+	
+	
+    
 
     return 0;
 }
