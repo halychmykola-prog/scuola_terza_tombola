06a0084f5df8d74a7d5058453c9013a396b10f0e
halychmykola-prog <halych.mykola.studente@itispaleocapa.it>
2026-01-12 21:14:07 +0100

    Add files via upload

--- tombola.cpp
+++ tombola.cpp
@@ -2,361 +2,172 @@
 #include <ctime>
 #include <cstdlib>
 #include <unistd.h>
+
 using namespace std;
 
 int main()
 {
-    //First part: tombola card generation
-    cout<<"Ciao, questo programma ti permette di giocare a Tombola. Numero massimo di giocatori: 10."<<endl;
-    cout<<"Inserisci il numero di giocatori: ";
-    int quantity_players;
-    cin>>quantity_players;
-    cout<<endl;
+ 
 
-    int cell[10][3][9]; 
-    srand(time(0));     
+    cout << "Ciao, questo programma ti permette di giocare a Tombola." << endl;
 
-    for(int i=0; i<quantity_players; i++)
+    int cell[2][3][9];
+    srand(time(0));
+
+
+    for(int i = 0; i < 2; i++)
     {
-
-        //Array to track used numbers in each column
         int used_numbers[9];
-        for(int c=0; c<9; c++)
-            used_numbers[c]=-1;
+        for(int c = 0; c < 9; c++)
+            used_numbers[c] = -1;
 
-        for(int row=0; row<3; row++)
+        for(int row = 0; row < 3; row++)
         {
-            int limit_number=0; // limit of numbers per row
+            
+            bool selected[9] = {false};
+            int chosen = 0;
 
-            for(int column=0; column<9; column++)
+            while(chosen < 5)
             {
-                
-                bool flag=rand()%2;
-                if(limit_number>=5) 
-                    flag=1;
-
-                switch(column)
+                int col = rand() % 9;
+                if(!selected[col])
                 {
-                    case 0:
-                        if(flag== 1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number = rand()%9+1;
-                            } while(number==used_numbers[column]);
+                    selected[col] = true;
+                    chosen++;
+                }
+            }
 
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 1:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+10;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 2:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+20;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 3:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+30;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }   
-                        break;
-
-                    case 4:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+40;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 5:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+50;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 6:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+60;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 7:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%10+70;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    case 8:
-                        if(flag==1)
-                        {
-                            cell[i][row][column]=-1;
-                        }
-                        else
-                        {
-                            int number;
-                            do
-                            {
-                                number=rand()%11+80;
-                            } while(number==used_numbers[column]);
-
-                            cell[i][row][column]=number;
-                            used_numbers[column]=number;
-                            limit_number++;
-                        }
-                        break;
-
-                    default:
-                        cout << "Something went wrong!" << endl;
-                        break;
+         
+            for(int column = 0; column < 9; column++)
+            {
+                if(!selected[column])
+                {
+                    cell[i][row][column] = -1;
+                    continue;
                 }
 
-            } // end for column iteration
-			
-        } // end for row iteration
+                int number;
+                do
+                {
+                    switch(column)
+                    {
+                        case 0: number = rand() % 9 + 1; break;
+                        case 1: number = rand() % 10 + 10; break;
+                        case 2: number = rand() % 10 + 20; break;
+                        case 3: number = rand() % 10 + 30; break;
+                        case 4: number = rand() % 10 + 40; break;
+                        case 5: number = rand() % 10 + 50; break;
+                        case 6: number = rand() % 10 + 60; break;
+                        case 7: number = rand() % 10 + 70; break;
+                        case 8: number = rand() % 11 + 80; break;
+                    }
+                } while(number == used_numbers[column]);
 
-        cout<<"Scheda del giocatore " <<i+1 <<endl;
-        for(int row=0; row< 3; row++)
+                cell[i][row][column] = number;
+                used_numbers[column] = number;
+            }
+        }
+
+        
+        cout << "Scheda del giocatore " << i + 1 << endl;
+        for(int row = 0; row < 3; row++)
         {
-            cout<<"------------------------------"<<endl;
-            
+            cout << "------------------------------" << endl;
             for(int col = 0; col < 9; col++)
             {
                 if(cell[i][row][col] == -1)
                     cout << "   ";
-                else if (cell[i][row][col] < 10 && cell[i][row][col] > 0)
+                else if(cell[i][row][col] < 10)
                     cout << cell[i][row][col] << "  ";
                 else
                     cout << cell[i][row][col] << " ";
             }
             cout << endl;
         }
-
         cout << endl;
-    } // end for players iteration
-
-
-    //help part
-    int count_numbers[10]={0};
-
-    for(int i=0; i<quantity_players; i++)
-    {
-        for(int row=0; row<3; row++)
-        {
-            for(int col=0; col<9; col++)
-            {
-                if(cell[i][row][col]!=-1)
-                {
-                    count_numbers[i]++;
-                }
-            }
-        }
-
-       
-
-
     }
 
-    //cout<<"1 player has "<<count_numbers[0]<<" numbers."<<endl;
-    //cout<<"1 player has "<<count_numbers[1]<<" numbers."<<endl;
+   
+    int count_numbers[2] = {0};
 
+    for(int i = 0; i < 2; i++)
+        for(int row = 0; row < 3; row++)
+            for(int col = 0; col < 9; col++)
+                if(cell[i][row][col] != -1)
+                    count_numbers[i]++;
 
-
-    //end of help part
-
-
-
-
-    //Second part: number extraction
-    cout<<"Are you ready to start the game? (y/n): ";
+   
+    cout << "Are you ready to start the game? (y/n): ";
     char ready;
-    cin>>ready;
-    if(ready!='y' && ready!='Y')
+    cin >> ready;
+
+    if(ready != 'y' && ready != 'Y')
     {
-        cout<<"Gioco finito"<<endl;
+        cout << "Gioco finito" << endl;
         return 0;
     }
 
-    /*else
+    int extracted_numbers[90] = {0};
+    bool winner_found = true;
+    int winner_player = -1;
+
+    while(winner_found)
     {
-        cout<<"\33[2J";
-    }*/
-    
+        int randnumber = rand() % 90 + 1;
+        if(extracted_numbers[randnumber - 1] != 0)
+            continue;
 
-    int extracted_numbers[90]={0};
-    bool winner_found=true;
-    int winner_player;
-    int randnumber;
+        extracted_numbers[randnumber - 1] = randnumber;
+        cout << "Numero estratto: " << randnumber << endl << endl;
 
-
-while(winner_found)
-{
-    randnumber=rand()%90+1;
-
-    if(extracted_numbers[randnumber-1]!=0)
-        continue;
-
-    cout <<"Numero estratto: "<<randnumber<<endl<<endl<<endl;
-    extracted_numbers[randnumber-1] =randnumber;
-
-    for(int i=0; i<quantity_players; i++)
-    {
-
-        for(int row=0; row<3; row++)
+        for(int i = 0; i < 2; i++)
         {
-            for(int col=0; col<9; col++)
+            for(int row = 0; row < 3; row++)
             {
-                if(cell[i][row][col]==randnumber)
+                for(int col = 0; col < 9; col++)
                 {
-                    cell[i][row][col]=0;
-                    count_numbers[i]--;
-
-                    if(count_numbers[i]==0)
+                    if(cell[i][row][col] == randnumber)
                     {
-                        winner_found=false;
-                        winner_player=i+1;
+                        cell[i][row][col] = 0;
+                        count_numbers[i]--;
+
+                        if(count_numbers[i] == 0)
+                        {
+                            winner_found = false;
+                            winner_player = i + 1;
+                        }
                     }
                 }
             }
+        }
 
-        
-        }// end for row iteration
-
-
-    }// end for players iteration
-
-    for(int i=0; i<quantity_players; i++)
-    {
-        cout<<"Scheda del giocatore " <<i+1 <<endl;
-        for(int row=0; row< 3; row++)
+      
+        for(int i = 0; i < 2; i++)
         {
-            cout<<"------------------------------"<<endl;
-            
-            for(int col = 0; col < 9; col++)
+            cout << "Scheda del giocatore " << i + 1 << endl;
+            for(int row = 0; row < 3; row++)
             {
-                if(cell[i][row][col] == -1)
-                    cout << "   ";
-                else if (cell[i][row][col] < 10 && cell[i][row][col] > 0)
-                    cout << cell[i][row][col] << "  ";
-                else
-                    cout << cell[i][row][col] << " ";
+                cout << "------------------------------" << endl;
+                for(int col = 0; col < 9; col++)
+                {
+                    if(cell[i][row][col] == -1)
+                        cout << "   ";
+                    else if(cell[i][row][col] == 0)
+                        cout << " X ";
+                    else if(cell[i][row][col] < 10)
+                        cout << cell[i][row][col] << "  ";
+                    else
+                        cout << cell[i][row][col] << " ";
+                }
+                cout << endl;
             }
             cout << endl;
         }
 
-        cout << endl;
-    } // end for players iteration
+        sleep(3);
+    }
 
-    cout<<endl<<endl;
-    sleep(3);
-
-
-}
-cout<<"Complimenti al giocatore "<<winner_player<<" che ha fatto tombola!"<<endl;
-
-
-
+    cout << "Complimenti al giocatore " << winner_player << " che ha fatto tombola!" << endl;
     return 0;
 }
+	
